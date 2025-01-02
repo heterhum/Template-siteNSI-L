3aa405765027b50e51ca18fdc0c44de2309721f0
heterhum <160785637+heterhum@users.noreply.github.com>
2025-01-02 20:22:38 +0100

    did something i think

--- Site_avec_back_end/api/index.js
+++ Site_avec_back_end/api/index.js
@@ -15,7 +15,9 @@ function runPythonScript(scriptPath, args, callback) {
     const pythonProcess = spawn('python', [scriptPath].concat(args)); //comme dans un terminale
     let data = '';
     pythonProcess.stdout.on('data', (chunk) => {
+        //console.log(chunk.toString());
         data += chunk.toString(); // Collect data from Python script
+        callback(null, data);
     });
     pythonProcess.stderr.on('data', (error) => { //gere les erreurs
         console.error(`stderr: ${error}`);
@@ -26,7 +28,7 @@ function runPythonScript(scriptPath, args, callback) {
             callback(`Error: Script exited with code ${code}`, null);
         } else {
             console.log('Python script executed successfully',data);
-            callback(null, data);
+            //callback(null, data);
         }
     });
 }
@@ -36,18 +38,23 @@ app.get('/', async(req, res) => {
     const indexHtml = await genererPageAccueil()
   res.send(indexHtml)
 })
-app.use('/styles',express.static('../public/styles'))
-app.use('/img',express.static('../public/img'))
+app.use((req, res, next) => {
+    res.set('X-Content-Type-Options', 'nosniff');
+    next();
+});
+
+app.use(express.static('./Site_avec_back_end/public'))
 
 //permet de recevoir et enovyé des json depuis /submit
 app.use(express.json())
 app.post('/submit', async (req, res) => {
     const data = await req.body; // Les données envoyées par le js public via fetch()
     console.log('Données reçues:', parseInt(data["value"]));
-    runPythonScript('../server/calcul.py', [parseInt(data["value"])], (err, result) => {
+    runPythonScript('./Site_avec_back_end/server/calcul.py', [parseInt(data["value"])], (err, result) => {
         if (err) {
             console.log(err); }
         else {
+            console.log('Données traitées:', result);
             res.json({
                 message: 'Données reçues et traitées avec succès',
                 receivedData: result
--- Site_avec_back_end/server/calcul.py
+++ Site_avec_back_end/server/calcul.py
@@ -15,7 +15,7 @@ import pygame
 
 running=True
 pygame.init()
-screen = pygame.display.set_mode((200, 200))
+screen = pygame.Surface((200, 200))
 clock = pygame.time.Clock()
 c=0
 while running:
@@ -23,6 +23,5 @@ while running:
     screen.fill((255, 255, 255))
     clock.tick(3)
     d=pygame.surfarray.pixels2d(screen)
-    print(d[0][1])
+    print(d)
     pygame.draw.rect(screen,(100,100,100),pygame.Rect(50+c, 50+c, 10, 10))
-    pygame.display.update()
