0eab1e2900b18625c9229ede6f117cc02d0f9718
heterhum <160785637+heterhum@users.noreply.github.com>
2025-01-29 21:24:45 +0100

    nop

--- test/script.js
+++ test/script.js
@@ -1,19 +1,28 @@
-window.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", function () {
 
-    const container = document.getElementById("image-container");
-    const image = document.getElementById('zoom-image');
-    const body = document.getElementById('d');
-
-    //body.style.bakcgroundcolor = "red";
+    let container = document.getElementById("image-container");
+    let image = document.getElementById('zoom-image');
+    let body = document.getElementById('d');
 
     container.addEventListener('mousemove', (e) => {
       const x = e.clientX 
       const y = e.clientY 
-
-      body.style.bakcgroundcolor = "red";
+      //body.style.backgroundColor = "red"; 
+      //for (let i =0; i < 180; i++){
+      //  image.style.transform = `rotateY(${i}deg), rotateZ(${i}deg)`;
+      //} 
+      
     });
 //container.addEventListener('mouseleave', () => {
 //
 //  image.style.transform = 'scale(1)';
 //});
 });
+
+
+//document.addEventListener('DOMContentLoaded', function () {
+//let = buttonRoll = document.getElementById("myButton")
+//buttonRoll.addEventListener("click", function(){
+//    buttonRoll.style.backgroundColor = "red";
+//});
+//});
