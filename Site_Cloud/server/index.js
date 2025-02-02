43f01b99b542ff851d37169bc9590208f6eb3a86
heterhum <160785637+heterhum@users.noreply.github.com>
2025-02-02 22:51:36 +0100

    DB can now be modify

--- Site_Cloud/server/index.js
+++ Site_Cloud/server/index.js
@@ -16,26 +16,69 @@ app.set("views", path.join(__dirname, "/Site_cloud/server/views"));
 app.set("view engine", "ejs");
 require('dotenv').config();
 
+const numbergen=17;
+const stringselec = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+function cookiegenerator(max) {
+  var l = ""
+  for (var i = 0; i < max; i++) {
+    l += stringselec[Math.floor(Math.random() * max)];
+  }
+  return l;
+}
 
 // Database
 const mongodb = require('mongodb'); 
 //const uri = process.env.URI;
 const client = new mongodb.MongoClient('mongodb://127.0.0.1:27017');
-async function main(client,name){ //TO DO : find a way to modify the database
+async function see_user_data(client,name){ 
   try {
       await client.connect();
-      const tofind= 'users.'+ name +'.exist';
-      var userdata =await client.db("myDB").collection("DB").find({[tofind]:true}).toArray();
-      //console.log(userdata)
-      let d = userdata[0]["users"][name]
+      var userdata =await client.db("myDB").collection("users").findOne({"username":name});
       await client.close();
-      return d;
+      if (userdata){
+        return userdata
+      } else {
+        return null
+      }
   } catch (e) {
       console.error(e);
-      console.log("l'utilisateur n'existe pas")
       await client.close();}
 };
-
+async function modifie_user_data(client,name,modifplace,modif){ 
+  const modification={[modifplace]:modif}
+  try {
+      await client.connect();
+      try {
+        await client.db("myDB").collection("users").updateOne({"username":name},{$set:modification});
+        await client.close();
+        return true
+      } catch (e) {
+        console.error(e);
+        await client.close();
+        return false
+      }
+  } catch (e) {
+      console.error(e);
+      await client.close();}
+};
+async function create_new_user(client,name,password){ //TO DO : allow new user creation
+  const modification={[modifplace]:modif}
+  try {
+      await client.connect();
+      try {
+        await client.db("myDB").collection("users").updateOne({"username":name},{$set:modification});
+        await client.close();
+        return true
+      } catch (e) {
+        console.error(e);
+        await client.close();
+        return false
+      }
+  } catch (e) {
+      console.error(e);
+      await client.close();}
+};
 
 // Upload file
 function permulter(userID){
@@ -56,6 +99,7 @@ function permulter(userID){
 app.get('/', async function(req, res) { // main page
   var filepath=path.join(__dirname,"Site_Cloud","public","main.html")
   res.sendFile(filepath);
+  console.log("someone is on the main page")
 });
 app.use('/static',express.static(__dirname+'/Site_cloud/public'));
 
@@ -64,43 +108,51 @@ app.use('/static',express.static(__dirname+'/Site_cloud/public'));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.post("/login", async function (req, res) { // TO DO : systeme de cookie pour la connexion
-  console.log("1")
   const password = req.body.connectpassword;
   const username = req.body.connectusername;
 
-  var data = await main(client,username).catch(console.error)
-  console.log("2")
-  console.log(data.password,password)
-  if (data.password==password){
+  var data = await see_user_data(client,username).catch(console.error)
+
+  if (data.password==password && data!=null){
     res.redirect('/user/'+username)
-    console.log("3")
+    console.log(username, " connected successfully")
   } else {
     res.status(204).send()
-    console.log("4")
+    console.log("someone try to connect but fail")
   }
 });
+app.post("/create", async function (req, res) { // TO DO : systeme de cookie pour la connexion
+  const password = req.body.createmdp;
+  const username = req.body.createname;
 
+  var data = await see_user_data(client,username).catch(console.error)
+
+  if (data.password==password && data!=null){
+    res.redirect('/user/'+username)
+    console.log(username, " connected successfully")
+  } else {
+    res.status(204).send()
+    console.log("someone try to connect but fail")
+  }
+});
 //--------------------------------------------------------------
 
 app.get('/user/:uid', async function(req, res,next) { // TO DO : systeme de cookie pour la connexion
   var uid = req.params.uid;
-  var data= await main(client,uid).catch(console.error);
-  console.log(uid,data)
-  console.log()
-  if (data!=undefined){
-    console.log("good")
+  var data= await see_user_data(client,uid).catch(console.error);
+  if (data!=null){
+    console.log(uid," is now on his own cloud")
     req.uide=uid;  
     res.render('HPuser',{"pp":data["pp"],"name":uid,"txt":data["file"]["01"]}); //Bien organiser
-    console.log(req.uide);
     next();
   } else{
     res.redirect('/');
+    console.log("connection with ",uid," failed")
   };
 });
 app.use('/static',express.static(__dirname+'/Site_cloud/server/views'),(req,res,next)=>{next()});
 app.use('/user/:uid/fluid', (req, res, next) => { // TO DO : Verif securité aprés systeme de cookie
   var uid=req.params.uid;
-  console.log(req.params.uid," good")
   express.static(__dirname+'/Site_cloud/public/Personnal_file/'+uid)(req,res,next); 
 });
 
@@ -109,9 +161,10 @@ app.use('/user/:uid/fluid', (req, res, next) => { // TO DO : Verif securité apr
 app.post('/upload/:uid', (req, res) => { // Ajouté sécurité, si l'utilisateur modifie le action post avec f12 alors il peut upload ou il veut
   permulter(req.params.uid).single('file')(req, res, function (err) {
     try {
-      console.log("it's working", req.params.uid)
+      console.log(req.params.uid, " successfully uploaded a file")
       res.status(204).send()
     } catch (err) {
+      console.log(req.params.uid, " fail to uploaded a file")
       res.status(400).send({ error: err.message });
     }
   });
