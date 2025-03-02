70f3e2c516c727718b4d446b149efc3c2223620c
heterhum <160785637+heterhum@users.noreply.github.com>
2025-03-02 12:54:48 +0100

    correction

--- test/script.js
+++ test/script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 const btna=document.getElementById("checkbox-41");
 const state1=`<div class="typewriter"><h1><span>ThereIsNo Cloud</span></h1></div><br><!-- From Uiverse.io by bociKond --><form class="form" action="/login" method="post"><span class="input-span"><label for="text" class="label">Username</label><input type="text" name="username" id="username"/></span><span class="input-span"><label for="password" class="label">Password</label><input type="password" name="password" id="password"/></span><span class="span"><a href="youtube.com">Forgot password?</a></span><input class="submit" type="submit" value="Send"/></form><div id="back"></div>`;
-const state2=`<div class="typewriter"><h1><span>ThereIsNo Cloud</span></h1></div><br><!-- From Uiverse.io by bociKond --><form class="form" action="/create" method="post"><span class="input-span"><label for="text" class="label">Username</label><input type="text" name="username" id="username"/></span><span class="input-span"><label for="password" class="label">Password</label><input type="password" name="password" id="password"/></span><input class="submit" type="submit" value="Create"/></form><div id="back"></div>
+const state2=`<div class="typewriter"><h1><span>ThereIsNo Cloud</span></h1></div><br><!-- From Uiverse.io by bociKond --><form class="form" action="/create" method="post"><span class="input-span"><label for="text" class="label">Username</label><input type="text" name="username" id="username"/></span><span class="input-span"><label for="password" class="label">Password</label><input type="password" name="password" id="password"/></span><input class="submit" type="submit" value="Send"/></form><div id="back"></div>
 `;
 let login=true;
 btna.addEventListener("change",(e)=>{
