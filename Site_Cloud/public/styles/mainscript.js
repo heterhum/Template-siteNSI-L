0bbaf46436891fbe56110e5be175fae8c0eb1f26
heterhum <160785637+heterhum@users.noreply.github.com>
2025-03-08 21:32:29 +0100

    ui

--- Site_Cloud/public/styles/mainscript.js
+++ Site_Cloud/public/styles/mainscript.js
@@ -1,3 +1,5 @@
+const socket = io();
+
 window.onload = function() {
     const btna=document.getElementById("checkbox-41");
     const state1=`<div class="typewriter"><h1><span>ThereIsNo Cloud</span></h1></div><br><!-- From Uiverse.io by bociKond --><form class="form" action="/login" method="post"><span class="input-span"><label for="text" class="label">Username</label><input type="text" name="connectusername" id="username"/></span><span class="input-span"><label for="password" class="label">Password</label><input type="password" name="connectpassword" id="password"/></span><span class="span"><a href="youtube.com">Forgot password?</a></span><input class="submit" type="submit" value="Send"/></form><div id="back"></div>`;
@@ -11,6 +13,9 @@ window.onload = function() {
         document.getElementById("connect1").innerHTML=state1
       }
       login=!login;
+    });
+    socket.on("creation",function(e){
+        if (!e){ console.log("creation failed")}
     });
     
     }
